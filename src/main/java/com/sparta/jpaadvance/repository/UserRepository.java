414d4c0975975fce0c98e81ad6d09c2b84dd19b3
shinmink <sinmk99@hansung.ac.kr>
2024-05-24 14:35:03 +0900

    지연 로딩과 즉시 로딩

--- src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
+++ src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.sparta.jpaadvance.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+
 }
