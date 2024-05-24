985e9e2c61d44f5eb95b3b8937be703f4bfd3216
shinmink <sinmk99@hansung.ac.kr>
2024-05-24 14:15:41 +0900

    중간 테이블 Order 주문일 추가 후 테스트

--- src/main/java/com/sparta/jpaadvance/JpaAdvanceApplication.java
+++ src/main/java/com/sparta/jpaadvance/JpaAdvanceApplication.java
@@ -2,7 +2,10 @@ package com.sparta.jpaadvance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JpaAdvanceApplication {
 
